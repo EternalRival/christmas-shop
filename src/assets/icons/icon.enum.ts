cd2a73b43f960cb32a404175ed42056956874b53
EternalRival <eternalrival8@gmail.com>
2024-10-31 20:50:19 +0300

    feat: replace text arrows with svg

--- src/assets/icons/icon.enum.ts
+++ src/assets/icons/icon.enum.ts
@@ -1,4 +1,7 @@
 export const enum Icon {
+  ARROW_LEFT = 'arrow-left',
+  ARROW_RIGHT = 'arrow-right',
+  ARROW_UP = 'arrow-up',
   FACEBOOK = 'facebook',
   INSTAGRAM = 'instagram',
   SNOWFLAKE = 'snowflake',
--- src/components/fab/index.ts
+++ src/components/fab/index.ts
@@ -1,7 +1,9 @@
 import { button } from '~/utils/create-element';
+import SVGIcon from '../svg-icon';
+import { Icon } from '~/assets/icons/icon.enum';
 
 const styles = { fab: '' };
 
 export default function FAB() {
-  return button({ className: styles['fab'] }, ['🡡']);
+  return button({ className: styles['fab'] }, [SVGIcon({ name: Icon.ARROW_UP })]);
 }
