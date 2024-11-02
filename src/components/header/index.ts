c3dc69693b5e796af3466d2438d2bcecfa275022
EternalRival <eternalrival8@gmail.com>
2024-11-02 03:52:11 +0300

    feat: add burger animation

--- src/components/header/index.ts
+++ src/components/header/index.ts
@@ -5,7 +5,48 @@ import { a, div, header, input, label, li, nav, ul } from '~/utils/create-elemen
 import isCurrentRoute from '~/utils/is-current-route';
 import styles from './header.module.css';
 
+function NavMenu({ linkList }: { linkList: { text: string; href: string }[] }) {
+  return nav({ className: styles['nav-menu'] }, [
+    ul(
+      { className: styles['nav-list'] },
+      linkList.map(({ text, href }) =>
+        li({ className: styles['nav-item'] }, [
+          a({
+            className: clsx(styles['nav-link'], 'text-action-small', isCurrentRoute(href) && styles['selected']),
+            href,
+            text,
+            inert: isCurrentRoute(href),
+          }),
+        ]),
+      ),
+    ),
+  ]);
+}
+
 export default function Header(props?: { logoUrl?: string; menuLinkList?: { text: string; href: string }[] }) {
+  const navMenu = props?.menuLinkList && NavMenu({ linkList: props.menuLinkList });
+  const burgerCheckbox = input({
+    className: styles['burger-checkbox'],
+    type: 'checkbox',
+    onchange: () => {
+      if (burgerCheckbox.checked) {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+    },
+  });
+
+  if (navMenu) {
+    const resizeObserver = new ResizeObserver((entries) => {
+      entries.forEach(({ contentRect }) => {
+        if (contentRect.width !== document.documentElement.clientWidth) {
+          burgerCheckbox.checked = false;
+        }
+      });
+    });
+
+    resizeObserver.observe(navMenu);
+  }
+
   return header({ className: styles['header'] }, [
     div({ className: styles['container'] }, [
       a({ className: clsx(styles['logo'], 'text-action-small'), ...(props?.logoUrl && { href: props.logoUrl }) }, [
@@ -13,24 +54,9 @@ export default function Header(props?: { logoUrl?: string; menuLinkList?: { text
         'the gifts',
       ]),
 
-      props?.menuLinkList &&
-        nav({ className: styles['nav-menu'] }, [
-          ul(
-            { className: styles['nav-list'] },
-            props.menuLinkList.map(({ text, href }) =>
-              li({ className: styles['nav-item'] }, [
-                a({
-                  className: clsx(styles['nav-link'], 'text-action-small', isCurrentRoute(href) && styles['selected']),
-                  href,
-                  text,
-                  inert: isCurrentRoute(href),
-                }),
-              ]),
-            ),
-          ),
-        ]),
+      navMenu,
 
-      label({ className: styles['burger'] }, [input({ className: styles['burger-checkbox'], type: 'checkbox' })]),
+      label({ className: styles['burger'] }, [burgerCheckbox]),
     ]),
   ]);
 }
