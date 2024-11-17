605174697d56dc8cc48a8007cd10ca9280afd1fd
EternalRival <eternalrival8@gmail.com>
2024-11-17 20:58:33 +0300

    fix: remove timezone offset for timer

--- src/modules/cta-section/utils/get-time-until-new-year-in-utc0.ts
+++ src/modules/cta-section/utils/get-time-until-new-year-in-utc0.ts
@@ -1,9 +1,7 @@
 export default function getTimeUntilNewYearInUTC0() {
   const now = new Date();
 
-  const offset = now.getTimezoneOffset() * 60 * 1000;
-
-  const nowTime = now.getTime() + offset;
+  const nowTime = now.getTime();
   const nextNewYearTime = Date.UTC(now.getUTCFullYear() + 1, 0, 1);
 
   return nextNewYearTime - nowTime;
